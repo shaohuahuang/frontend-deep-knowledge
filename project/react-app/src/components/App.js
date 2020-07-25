7780754d5ab17b6e091acad86ffe817e0a93ec3b
Shaohua Huang <hshnju@gmail.com>
2020-07-25 14:25:42 +0800

    add router

--- project/react-app/src/components/App.js
+++ project/react-app/src/components/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { Link } from 'react-router-dom'
 
 function App() {
   return (
@@ -18,7 +19,9 @@ function App() {
         >
           Learn React
         </a>
+        <Link to="./profile">To Profile</Link>
       </header>
+
     </div>
   );
 }
--- project/react-app/src/index.js
+++ project/react-app/src/index.js
@@ -1,17 +1,21 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from 'react'
+import { render } from 'react-dom'
+import { createStore } from 'redux'
+import { Provider } from 'react-redux'
+import App from './components/App'
+import Profile from './components/Profile'
+import rootReducer from './reducers'
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+import { BrowserRouter as Router, Route } from 'react-router-dom'
+
+const store = createStore(rootReducer)
+
+render(
+  <Provider store={store}>
+    <Router>
+      <Route path="/" component={App} />
+      <Route path="/profile" component={Profile} />
+    </Router>
+  </Provider>,
   document.getElementById('root')
-);
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+)
